fb4d0f95ae7532e05a918e72c3c0435bc6a7a116
duran89 <leral3@naver.com>
2024-03-26 20:08:02 +0900

    feat 조거문

---
+++ Swift_Tutorial/if-else.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+let myNumber: Int = 100
+//
+//if myNumber < 100 {
+//    print("100 보다 작아요")
+//} else if myNumber > 100 {
+//    print("100 보다 커요")
+//} else {
+//    print("100이에요!")
+//} // 100이에요
+
+
+//switch myNumber {
+//case 0...30:
+//    print("0 ~ 30 사이에 있습니다.")
+//case 31...60:
+//    print("31 ~ 60 사이에 있습니다.")
+//case 61...99:
+//    print("61 ~ 99 사이에 있습니다.")
+//default:
+//    print("100이에요!!")
+//} // 100이에요!!
+//
+
+//let myName: String = "jerry"
+//
+//switch myName {
+//case "mike":
+//    print("제 이름은 mike에요")
+//case "john":
+//    print("제 이름은 john에요")
+//default:
+//    print("jerry에요")
+//} // jerry에요
